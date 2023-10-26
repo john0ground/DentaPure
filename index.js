b51cc0ab9668cce884c235bdb338128b6ac067d5
John <mjohncorbet@gmail.com>
2023-10-27 00:31:11 +0800

    set tab order trap focus for nav bar

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const btnToggleNav = document.querySelector('.btn-toggle-nav');
 const nav = document.querySelector('nav');
-const navLinks = document.querySelectorAll('nav ul li a'); 
+const navLinks = document.querySelectorAll('nav ul li a');
+const navBookLink = document.querySelector('nav #link-book');
 
 let displayNav = false;
+let mobileScreen = false;
 
 function toggleNavFocus(state) {
     state === true?
@@ -27,9 +29,18 @@ function toggleNavBar(e) {
 btnToggleNav.addEventListener('click', toggleNavBar);
 btnToggleNav.addEventListener('keydown', toggleNavBar);
 
+//  tab order trap focus if nav bar is displayed.
+navBookLink.addEventListener('keydown', (e) => {
+    if (e.key === "Tab" && mobileScreen) {
+        e.preventDefault();
+        btnToggleNav.focus();
+    }
+});
+
 //  initially hides the tab order for the nav links if the screen is mobile.
 (function navTabIndex() {
     if (window.innerWidth <= 700) {
         toggleNavFocus(false);
+        mobileScreen = true;
     }
 })();
