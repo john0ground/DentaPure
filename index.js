226cea23bd9a80c7fe2fe93f3de03ab9ff184297
John <mjohncorbet@gmail.com>
2023-10-26 23:08:27 +0800

    modify nav tab order for small screens

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const btnToggleNav = document.querySelector('.btn-toggle-nav');
 const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('nav ul li a'); 
 
 let displayNav = false;
 
-function toggleNav(e) {
+function toggleNavFocus(state) {
+    state === true?
+    navLinks.forEach(link => link.removeAttribute('tabindex')):
+    navLinks.forEach(link => link.setAttribute('tabindex', '-1'));
+}
+
+function toggleNavBar(e) {
     if (e.type === 'click' || e.key === ' ' || e.key === 'Enter') {
         e.preventDefault();
 
@@ -13,8 +20,16 @@ function toggleNav(e) {
         btnToggleNav.setAttribute('aria-label', 'open navigation links');
 
         nav.setAttribute('data-display', displayNav.toString());
+        toggleNavFocus(displayNav);
     }
 }
 
-btnToggleNav.addEventListener('click', toggleNav);
-btnToggleNav.addEventListener('keydown', toggleNav);
+btnToggleNav.addEventListener('click', toggleNavBar);
+btnToggleNav.addEventListener('keydown', toggleNavBar);
+
+//  initially hides the tab order for the nav links if the screen is mobile.
+(function navTabIndex() {
+    if (window.innerWidth <= 700) {
+        toggleNavFocus(false);
+    }
+})();
